b9271b6e56d58f3de9a35e89a6ab2f39f71db99f
Orlando Chalo <orlando@oliver.pet>
2024-12-27 14:18:53 -0300

    login-feature feat: add response format class

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * In order to keep the response format consistent, we can create a helper class that will handle the response format.
+ */
+class ApiResponse
+{
+    /**
+     * success
+     *
+     * @param  array $data
+     * @param  string $message
+     * @param  int $status
+     * @return JsonResponse
+     */
+    public static function success(array $data = [], string $message = 'success', int $status = 200): JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+
+    /**
+     * error
+     *
+     * @param  string $message
+     * @param  array $errors
+     * @param  int $status
+     * @return JsonResponse
+     */
+    public static function error(string $message = 'An error has occurred', array $errors = [], int $status = 400): JsonResponse
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
+}
