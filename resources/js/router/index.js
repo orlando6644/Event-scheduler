80b3ae499ac3a50313a8236a4b431bc0eb63db8b
Orlando Chalo <orlando@oliver.pet>
2024-12-28 09:37:02 -0300

    login-feature fix: modify router to protect routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,10 +4,6 @@ import store from '../store';
 import Login from '../components/auth/Login.vue';
 import Dashboard from '../components/dashboard/Dashboard.vue';
 
-const isAuthenticated = () => {
-    return store.getters.isAuthenticated;
-}
-
 const routes = [
     {
         path: '/',
@@ -17,31 +13,47 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: Login,
-        beforeEnter: (to, from, next) => {
-            if (isAuthenticated()) {
-                next({ name: 'Dashboard' });
-            } else {
-                next();
-            }
-        }
+        meta: { requiresAuth: false },
     },
     {
         path: '/dashboard',
         name: 'Dashboard',
         component: Dashboard,
-        beforeEnter: (to, from, next) => {
-            if (!isAuthenticated()) {
-                next({ name: 'Login' });
-            } else {
-                next();
-            }
-        }
+        meta: { requiresAuth: true },
     }
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_URL || '/'),
-  routes
+    history: createWebHistory('/'),
+    routes
 });
+
+router.beforeEach(async (to, from, next) => {
+
+    if (to.meta.requiresAuth && !store.getters['auth/isAuthenticated']) {
+      await store.dispatch('auth/fetchUser');
+
+      if (!store.getters['auth/isAuthenticated']) {
+        next({ name: 'Login' });
+      } else {
+        next();
+      }
+
+    } else if (to.name === 'Login') {
+        if (store.getters['auth/isAuthenticated']) {
+            next({ name: 'Dashboard' });
+        } else {
+            await store.dispatch('auth/fetchUser');
+
+            if (store.getters['auth/isAuthenticated']) {
+                next({ name: 'Dashboard' });
+            } else {
+                next();
+            }
+        }
+    } else {
+        next();
+    }
+  });
 
 export default router;
